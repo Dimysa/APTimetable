a9b01214d4ccdc4d3d86f43530bb5307784e3ffc
Dimysa <dimysa30@outlook.com>
2017-05-23 20:49:19 +0300

    fix print edit table

--- src/main/java/com/example/controllers/api/DisciplinesController.java
+++ src/main/java/com/example/controllers/api/DisciplinesController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
